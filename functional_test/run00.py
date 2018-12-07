8f0781062ff60a57ef076c0e536a4e8b8c71eb6e
Martin Kielhorn <kielhorn.martin@gmail.com>
2018-12-07 22:44:48 +0100

    how to allow camera access from functional test?

--- functional_test/run00.py
+++ functional_test/run00.py
@@ -8,8 +8,9 @@ import selenium.webdriver.common.keys
 
 driver=selenium.webdriver.Firefox()
 myip=socket.gethostbyname(socket.gethostname())
-url="https://{}/".format(myip)
+url="https://{}:7777/".format(myip)
 
 print("open url={}".format(url))
 driver.get(url)
+driver.find_element_by_tag_name("html").send_keys(((selenium.webdriver.common.keys.Keys.ALT)+("a")))
 
--- gen-functional-test.lisp
+++ gen-functional-test.lisp
@@ -13,15 +13,19 @@
 		   pathlib
 		   socket
 		   selenium
-		   selenium.webdriver.common.keys))
+		   selenium.webdriver.common.keys
+		   ))
 	 (do0
 	  (setf driver (selenium.webdriver.Firefox)
 		myip (socket.gethostbyname (socket.gethostname))
-		url (dot (string "https://{}/")
-			   (format myip)))
+		url (dot (string "https://{}:7777/")
+			 (format myip))
+		)
 	  (print (dot (string "open url={}")
 		      (format url)))
-	  (driver.get url))
+	  (driver.get url)
+	  (dot (driver.find_element_by_tag_name (string "html"))
+	       (send_keys (+ selenium.webdriver.common.keys.Keys.ALT (string "a"))) ))
 	 )))
   ;(run code)
   (write-source "/home/martin/stage/html5_surveillance/functional_test/run00" code))
